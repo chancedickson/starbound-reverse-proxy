50b275148cc7f2b4ddd00d38d52fdcf9c69084c4
Chance Dickson <chance@glitch.com>
2019-06-10 18:14:30 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+import { execSync } from "child_process";
+import * as net from "net";
+
+const {
+  SRP_TIMEOUT: timeoutLength,
+  SRP_PROXY_HOSTNAME: proxyHostname = "localhost",
+  SRP_PROXY_PORT: proxyPort,
+  SRP_PORT: port
+} = process.env;
+
+const server = net.createServer();
+
+const stopServer = () => execSync("poweroff");
+
+const buildConnectionHandler = () => {
+  let connections = 0;
+  let closeTimeout = null;
+
+  const buildCloseEventHandler = () => {
+    let closed = false;
+
+    return (sock) => () => {
+      if (!closed) {
+        closed = true;
+        connections -= 1;
+        sock.end();
+
+        if (connections === 0) {
+          closeTimeout = setTimeout(stopServer, timeoutLength);
+        }
+      }
+    };
+  };
+
+  return (socket) => {
+    connections += 1;
+    clearTimeout(closeTimeout);
+    const connection = net.createConnection(proxyPort, proxyHostname);
+    socket.pipe(connection);
+    connection.pipe(socket);
+
+    const closeIfNotClosed = buildCloseEventHandler();
+    socket.on("error", closeIfNotClosed(connection));
+    socket.on("close", closeIfNotClosed(connection));
+    connection.on("error", closeIfNotClosed(socket));
+    connection.on("close", closeIfNotClosed(socket));
+  };
+};
+
+server.on("connection", buildConnectionHandler());
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
